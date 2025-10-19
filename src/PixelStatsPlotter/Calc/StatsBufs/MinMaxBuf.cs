55298cb173234ac3a844da21ea9d3b2b3cd05d23
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-20 07:17:59 +0800

    构建最值缓冲区

---
+++ src/PixelStatsPlotter/Calc/StatsBufs/MinMaxBuf.cs
@@ -0,0 +1,34 @@
+﻿using OpenCvSharp;
+
+namespace PixelStatsPlotter.Calc.StatsBufs;
+
+/// <summary> 像素最值缓冲区 </summary>
+/// <param name="tgtCh"> 目标通道 </param>
+internal sealed class MinMaxBuf(Enums.ImgCh tgtCh) : IStatsBuf
+{
+    /// <summary> 最小值序列 </summary>
+    private readonly List<double> _minBuf = [];
+
+    /// <summary> 最大值序列 </summary>
+    private readonly List<double> _maxBuf = [];
+
+    public void Collect(Mat img) {
+        var type = img.Type();
+        if (tgtCh == Enums.ImgCh.A
+            && type.Channels != 4) {
+            _minBuf.Add(1); // 非4通道图的A值视为1
+            _maxBuf.Add(1);
+        } else {
+            using var tgtMat = img.GetCh(tgtCh, type.Channels);
+            tgtMat.MinMaxIdx(out var min, out var max);
+            var normMin = min.Norm01(type.Depth);
+            var normMax = max.Norm01(type.Depth);
+            _minBuf.Add(normMin);
+            _maxBuf.Add(normMax);
+        }
+    }
+
+    public (string Name, double[] Values)[] Snapshot()
+        => [($"{tgtCh}_Min", _minBuf.ToArray()),
+            ($"{tgtCh}_Max", _maxBuf.ToArray())];
+}
