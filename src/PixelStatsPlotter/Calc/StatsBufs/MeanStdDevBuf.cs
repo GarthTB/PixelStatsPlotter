7138ce55447d345d5dcd08566559d2397a7ce3cb
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-20 07:24:44 +0800

    构建均值与标准差缓冲区

---
+++ src/PixelStatsPlotter/Calc/StatsBufs/MeanStdDevBuf.cs
@@ -0,0 +1,37 @@
+﻿using OpenCvSharp;
+
+namespace PixelStatsPlotter.Calc.StatsBufs;
+
+/// <summary> 像素均值与标准差缓冲区 </summary>
+/// <param name="tgtCh"> 目标通道 </param>
+internal sealed class MeanStdDevBuf(Enums.ImgCh tgtCh) : IStatsBuf
+{
+    /// <summary> 均值序列 </summary>
+    private readonly List<double> _meanBuf = [];
+
+    /// <summary> 标准差序列 </summary>
+    private readonly List<double> _stdDevBuf = [];
+
+    public void Collect(Mat img) {
+        var type = img.Type();
+        if (tgtCh == Enums.ImgCh.A
+            && type.Channels != 4) {
+            _meanBuf.Add(1); // 非4通道图的A值视为1，标准差为0
+            _stdDevBuf.Add(0);
+        } else {
+            using Mat meanMat = new(), stdDevMat = new();
+            using var tgtMat = img.GetCh(tgtCh, type.Channels);
+            tgtMat.MeanStdDev(meanMat, stdDevMat);
+            var mean = meanMat.At<double>(0);
+            var stdDev = stdDevMat.At<double>(0);
+            var normMean = mean.Norm01(type.Depth);
+            var normStdDev = stdDev.Norm01StdDev(type.Depth);
+            _meanBuf.Add(normMean);
+            _stdDevBuf.Add(normStdDev);
+        }
+    }
+
+    public (string Name, double[] Values)[] Snapshot()
+        => [($"{tgtCh}_Mean", _meanBuf.ToArray()),
+            ($"{tgtCh}_StdDev", _stdDevBuf.ToArray())];
+}
