3c7038e66f59efabdc3319304eae67e4999e7249
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-19 10:40:04 +0800

    构建枚举

---
+++ src/PixelStatsPlotter/Enums/ImgCh.cs
@@ -0,0 +1,5 @@
+﻿namespace PixelStatsPlotter.Enums;
+
+/// <summary> 图像通道 </summary>
+internal enum ImgCh
+{ R, G, B, A }
---
+++ src/PixelStatsPlotter/Enums/Stat.cs
@@ -0,0 +1,13 @@
+﻿namespace PixelStatsPlotter.Enums;
+
+/// <summary> 统计量 </summary>
+[Flags] // 用于归并
+internal enum Stat
+{
+    Max = 1,
+    Mean = 2,
+    Min = 4,
+    StdDev = 8,
+    MinMax = Min | Max,
+    MeanStdDev = Mean | StdDev
+}
