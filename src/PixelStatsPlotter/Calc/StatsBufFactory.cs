bfd000a18316eb1cbefb2718f18197de2423cc8a
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-20 08:11:36 +0800

    完成工厂

--- src/PixelStatsPlotter/Calc/StatsBufFactory.cs
+++ src/PixelStatsPlotter/Calc/StatsBufFactory.cs
@@ -1,12 +1,35 @@
-﻿using PixelStatsPlotter.Enums;
+﻿using PixelStatsPlotter.Calc.StatsBufs;
+using PixelStatsPlotter.Enums;
 
 namespace PixelStatsPlotter.Calc;
 
-/// <summary> IStatsBuf实现类的工厂 </summary>
+/// <summary> 像素值统计结果缓冲区工厂 </summary>
 internal static class StatsBufFactory
 {
-    /// <summary> 根据通道和统计量创建IStatsBuf实现类 </summary>
-    public static IStatsBuf Create(ImgCh ch, Stat stat)
-        => stat switch {
-        };
+    /// <summary> 依据通道和统计量创建缓冲区 </summary>
+    /// <param name="statsPerCh"> 按通道分组的统计量 </param>
+    /// <returns> 已初始化的缓冲区实例数组 </returns>
+    /// <remarks> 每个通道的所有统计量需提前用位运算合并 </remarks>
+    public static IStatsBuf[] Create((ImgCh TgtCh, Stat Stats)[] statsPerCh)
+        => [.. statsPerCh.SelectMany(tup => {
+            List<IStatsBuf> bufs = new(3); // 每通道最多3个buf
+            var (ch, stats) = tup;
+            if (stats.HasFlag(Stat.MeanStdDev)) {
+                bufs.Add(new MeanStdDevBuf(ch));
+                stats &= ~Stat.MeanStdDev;
+            }
+            if (stats.HasFlag(Stat.MinMax)) {
+                bufs.Add(new MinMaxBuf(ch));
+                stats &= ~Stat.MinMax;
+            }
+            if (stats.HasFlag(Stat.Max))
+                bufs.Add(new MaxBuf(ch));
+            if (stats.HasFlag(Stat.Mean))
+                bufs.Add(new MeanBuf(ch));
+            if (stats.HasFlag(Stat.Min))
+                bufs.Add(new MinBuf(ch));
+            if (stats.HasFlag(Stat.StdDev))
+                bufs.Add(new StdDevBuf(ch));
+            return bufs;
+        })];
 }
