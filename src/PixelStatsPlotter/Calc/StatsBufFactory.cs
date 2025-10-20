aff96a8fe7f788c79e9d208e00406c08f5442919
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-20 19:47:03 +0800

    修复工厂方法

--- src/PixelStatsPlotter/Calc/StatsBufFactory.cs
+++ src/PixelStatsPlotter/Calc/StatsBufFactory.cs
@@ -10,28 +10,26 @@ internal static class StatsBufFactory
     /// <param name="statsPerCh"> 按通道分组的统计量 </param>
     /// <returns> 已初始化的缓冲区实例数组 </returns>
     /// <remarks> 每个通道的所有统计量需提前用位运算合并 </remarks>
-    public static IStatsBuf[] Create((ImgCh TgtCh, Stat Stats)[] statsPerCh)
-        => [.. statsPerCh.Aggregate(
-            new List<IStatsBuf>(),
-            static (bufs, tup) => {
-                var (tgtCh, stats) = tup;
-
-                if (stats.HasFlag(Stat.MeanStdDev)) {
-                    bufs.Add(new MeanStdDevBuf(tgtCh));
-                    stats &= ~Stat.MeanStdDev;
-                } else if (stats.HasFlag(Stat.Mean))
+    public static IStatsBuf[] Create((ImgCh TgtCh, Stat Stats)[] statsPerCh) {
+        List<IStatsBuf> bufs = [];
+        foreach (var (tgtCh, stats) in statsPerCh) {
+            if (stats.HasFlag(Stat.MeanStdDev)) {
+                bufs.Add(new MeanStdDevBuf(tgtCh));
+            } else {
+                if (stats.HasFlag(Stat.Mean))
                     bufs.Add(new MeanBuf(tgtCh));
-                else if (stats.HasFlag(Stat.StdDev))
+                if (stats.HasFlag(Stat.StdDev))
                     bufs.Add(new StdDevBuf(tgtCh));
-
-                if (stats.HasFlag(Stat.MinMax)) {
-                    bufs.Add(new MinMaxBuf(tgtCh));
-                    stats &= ~Stat.MinMax;
-                } else if (stats.HasFlag(Stat.Max))
+            }
+            if (stats.HasFlag(Stat.MinMax)) {
+                bufs.Add(new MinMaxBuf(tgtCh));
+            } else {
+                if (stats.HasFlag(Stat.Max))
                     bufs.Add(new MaxBuf(tgtCh));
-                else if (stats.HasFlag(Stat.Min))
+                if (stats.HasFlag(Stat.Min))
                     bufs.Add(new MinBuf(tgtCh));
-
-                return bufs;
-            })];
+            }
+        }
+        return [.. bufs];
+    }
 }
