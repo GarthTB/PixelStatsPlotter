896aec178268b1ba0aad8c2eda58998803951ab3
GarthTB | 天卜 <g-art-h@outlook.com>
2025-10-21 14:51:16 +0800

    构建视频处理器

--- src/PixelStatsPlotter/Proc/ImageProc.cs
+++ src/PixelStatsPlotter/Proc/ImageProc.cs
@@ -7,20 +7,21 @@ internal static class ImageProc
 {
     public static (string Name, double[] Values)[] Run(Config.ConfigModel config) {
         var (start, count) = config.Range;
-        var pathsSpan = (start, count) is ( >= 0, > 0)
+        var pathsSpan = start >= 0
+            && count > 0
             && start + count <= config.Paths.Length
             ? config.Paths.AsSpan(start, count)
             : config.Paths.AsSpan(); // 无效则测量全部
 
         Console.WriteLine($"共 {pathsSpan.Length} 张图像。开始处理...");
         foreach (var path in pathsSpan) {
-            using Mat mat = new(path, ImreadModes.Unchanged);
-            using var roi = config.GetRoi(mat);
+            using Mat img = new(path, ImreadModes.Unchanged);
+            using var roi = config.GetRoi(img);
             foreach (var buf in config.StatsBufs)
                 buf.Collect(roi);
         }
         Console.WriteLine("处理完成！");
 
-        return [.. config.StatsBufs.SelectMany(buf => buf.Snapshot())];
+        return [.. config.StatsBufs.SelectMany(static buf => buf.Snapshot())];
     }
 }
---
+++ src/PixelStatsPlotter/Proc/VideoProc.cs
@@ -0,0 +1,43 @@
+﻿using OpenCvSharp;
+
+namespace PixelStatsPlotter.Proc;
+
+/// <summary> 视频处理器 </summary>
+internal static class VideoProc
+{
+    public static (string Name, double[] Values)[] Run(Config.ConfigModel config) {
+        var file = config.Paths.FirstOrDefault()
+            ?? throw new ArgumentException(
+                "未指定视频文件路径", nameof(config));
+        using VideoCapture capture = new(file);
+        if (!capture.IsOpened())
+            throw new InvalidOperationException(
+                $"无法打开视频文件 `{file}`");
+
+        Console.WriteLine("开始处理视频...");
+        using (Mat frame = new())
+            if (config.Range is ( >= 0, > 0) range) {
+                if (!capture.Set(VideoCaptureProperties.PosFrames, range.Start))
+                    throw new InvalidOperationException(
+                        $"无法设置起始帧号 `{range.Start}`");
+                for (var cnt = 0; cnt < range.Count; cnt++)
+                    if (capture.Read(frame))
+                        CollectFrame(frame);
+                    else {
+                        Console.WriteLine($"第 {cnt}/{range.Count} 帧超出结尾。");
+                        break;
+                    }
+            } else
+                while (capture.Read(frame))
+                    CollectFrame(frame);
+        Console.WriteLine("处理完成！");
+
+        return [.. config.StatsBufs.SelectMany(static buf => buf.Snapshot())];
+
+        void CollectFrame(Mat frame) {
+            using var roi = config.GetRoi(frame);
+            foreach (var buf in config.StatsBufs)
+                buf.Collect(roi);
+        }
+    }
+}
